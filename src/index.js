dd448a80dcefca3592fbd505e233e4ad4dc1688a
wardxela <axelwardweb@gmail.com>
2021-08-12 16:16:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+// Stupid solution
+// module.exports = reverse = (n) => parseInt(n.toString().split("").reverse().join(""));
+
+// Just a bit math :)
+module.exports = reverse = (n) => {
+    if (n < 0) n = Math.abs(n);
+    if (n < 10) return `${n}`;
+    return `${n % 10}${reverse(Math.floor(n / 10))}`;
+};
